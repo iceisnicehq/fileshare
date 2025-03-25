7a3fee37ef1cc8050ff560138b65636653ab7e8c
Sab <66420569+iceisnicehq@users.noreply.github.com>
2025-03-25 14:37:39 +0300

    Create random.sh

---
+++ random.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Function to generate a random ANSI color code (bold, bright colors)
+random_color() {
+    colors=("31" "32" "33" "34" "35" "36" "91" "92" "93" "94" "95" "96")
+    echo -e "\033[1;${colors[$((RANDOM % ${#colors[@]}))]}m"
+}
+
+# Reset terminal color
+reset_color="\033[0m"
+
+# Main loop
+while true; do
+    color=$(random_color)
+    echo -e "${color}hi${reset_color}"
+    sleep 3
+done
