716772ee9dee289202fef4bcffaa6b056cbcf3ea
Sab <66420569+iceisnicehq@users.noreply.github.com>
2025-03-25 15:58:16 +0300

    Update lr.cpp

--- lr.cpp
+++ lr.cpp
@@ -56,3 +56,13 @@ signals:
 };
 
 #endif // DATA_PROCESSOR_H
+
+
+///
+
+#include "data_processor.h"
+
+void DataProcessor::processData(const QString &input) {
+    QString processedData = "Обработано: " + input;
+    emit dataProcessed(processedData);
+}
