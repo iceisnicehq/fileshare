0b8c940aa255d02910e7429c1724e6b82b685bc2
Sab <66420569+iceisnicehq@users.noreply.github.com>
2025-03-25 16:04:48 +0300

    Update lr.cpp

--- lr.cpp
+++ lr.cpp
@@ -93,3 +93,40 @@ private:
 };
 
 #endif // MAINWINDOW_H
+
+
+
+///
+
+
+
+#include "mainwindow.h"
+#include <QVBoxLayout>
+#include <QWidget>
+
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
+    QWidget *centralWidget = new QWidget(this);
+    setCentralWidget(centralWidget);
+
+    inputField = new QLineEdit(this);
+    processButton = new QPushButton("Обработать", this);
+    outputLabel = new QLabel("Результат: ", this);
+
+    QVBoxLayout *layout = new QVBoxLayout(centralWidget);
+    layout->addWidget(inputField);
+    layout->addWidget(processButton);
+    layout->addWidget(outputLabel);
+
+    processor = new DataProcessor(this);
+
+    connect(processor, &DataProcessor::dataProcessed, this, &MainWindow::handleProcessedData);
+    connect(processButton, &QPushButton::clicked, this, &MainWindow::onProcessButtonClicked);
+}
+void MainWindow::handleProcessedData(const QString &data) {
+    outputLabel->setText("Результат: " + data);
+}
+
+void MainWindow::onProcessButtonClicked() {
+    QString inputData = inputField->text();
+    processor->processData(inputData);
+}
